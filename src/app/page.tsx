623175749218b2243b58f8e95093622a9a28b711
Sanshiroh SAKAI <sansyo@gmail.com>
2023-12-23 15:05:04 +0900

    prisma の導入とサンプルデータ出力

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-const Home = () => {
-  return <div>Enter</div>
+import prisma from '@/lib/prisma'
+
+const Home = async () => {
+  const todos = await prisma.todo.findMany()
+
+  return (
+    <div className="m-8">
+      <h1 className="text-xl font-bold">Todo 一覧</h1>
+      <ul className="mt-8">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Home
